2c0fd47e2ff5227c306efba99876586efdac414f
Anna W <anski.anna@gmail.com>
2019-06-21 08:32:01 +0100

    added action types and action creators

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,40 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
 
+export const FETCHING_SMURFS_START = "FETCHING_SMURFS_START";
+export const FETCHING_SMURFS_SUCCESS = "FETCHING_SMURFS_SUCCESS";
+export const FETCHING_SMURFS_FAILURE = "FETCHING_SMURFS_FAILURE";
+
+export const ADD_SMURFS_START = "ADD_SMURFS_START";
+export const ADD_SMURFS_SUCCESS = "ADD_SMURFS_SUCCESS";
+export const ADD_SMURFS_FAILURE = "ADD_SMURFS_FAILURE";
+
+
+export const getSmurfs = () => dispatch => {
+  dispatch({ type: FETCHING_SMURFS_START });
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res=> {
+      dispatch({ type: FETCHING_SMURFS_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+      dispatch({ type: FETCHING_SMURFS_FAILURE, payload: err })
+    })
+}
+
+export const addSmurf = (smurf) => dispatch => {
+  dispatch({ type: ADD_SMURFS_START });
+  axios
+    .post('http://localhost:3333/smurfs', smurf)
+    .then(res => {
+      dispatch({ type: ADD_SMURFS_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+      dispatch({ type: ADD_SMURFS_FAILURE, payload: err })
+    })
+}
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
